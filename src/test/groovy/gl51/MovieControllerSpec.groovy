74c9d418dc1e27a3c308d74618be0776872c8364
jinhogate <kossi.gbegnon@utbm.fr>
2020-04-10 15:44:50 +0200

    integration du test du controller

--- src/test/groovy/gl51/MovieControllerSpec.groovy
+++ src/test/groovy/gl51/MovieControllerSpec.groovy
@@ -1,12 +1,25 @@
 package gl51
 
 import gl51.movie.data.Movie
+import gl51.movie.data.MovieRequest
 import gl51.movie.service.MovieClient
 import gl51.movie.service.impl.MovieClientImpl
-import gl51.movie.service.impl.MovieRegistryImpl
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.test.annotation.MockBean
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import spock.lang.AutoCleanup
 import spock.lang.Specification
+import spock.lang.Shared
+
+import javax.inject.Inject
 
 
 import javax.inject.Inject
@@ -14,28 +27,28 @@ import javax.inject.Inject
 @MicronautTest
 class MovieControllerSpec extends Specification {
 
-    @Inject
-    MovieRegistryImpl registry
+    @Shared @Inject
+    EmbeddedServer embeddedServer
 
-    void "injection should work"() {
+    @Shared @AutoCleanup @Inject @Client("/")
+    RxHttpClient client
+
+    void "test index"() {
+        given:
+        Flowable flowable = client.retrieve(HttpRequest.GET("/movie"), Argument.listOf(Movie))
+        def content = flowable.firstElement()
         expect:
-        registry != null
+        content.blockingGet() == []
     }
 
-    void "favorites should be empty"() {
+    void "test film creation"() {
+        given:
+        HttpResponse response = client.toBlocking().exchange(
+                HttpRequest.POST("/movie", new MovieRequest(imdbId: "aaaaa"))
+        )
         expect:
-        registry.listFavorites() == []
+        response.status == HttpStatus.CREATED
     }
-
-    void "adding a favorite should fill in the database"() {
-        when:
-        registry.addMovieToFavorites("aaaaa")
-        then:
-        registry.listFavorites().size() == 1
-        registry.listFavorites().find { it.title == 'my movie'}
-    }
-
-
     @MockBean(MovieClientImpl)
     MovieClient movieClient() {
         def mock = Mock(MovieClient)
